161561248db96bfa07debddc719d91cbdaadf203
Evan <evanbennett83@gmail.com>
2023-11-25 11:44:00 -0500

    created public folder. Relocated existing index and assets folder into public folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const PORT = 3001
 
+app.use(express.static('public'))
+
 app.listen(PORT, () => {
-  console.log(`Server running https://localhost:${PORT}`)
+  console.log(`Server running http://localhost:${PORT}`)
 })
