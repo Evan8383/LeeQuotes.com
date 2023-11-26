9149ff016d5f4548c6b5802c87cb9d6c5a0700ce
Evan <evanbennett83@gmail.com>
2023-11-26 16:14:35 -0500

    set up post request options. setup a variable to capture the information passed through the post request.

--- public/assets/js/quote-form.js
+++ public/assets/js/quote-form.js
@@ -1,9 +1,24 @@
 // GLOBAL VARIABLES
 const goBackBtn = document.getElementById('go-back-btn');
 const listContainer = document.getElementById('listContainer');
+const quoteForm = document.getElementById('quoteForm');
+const quoteInput = document.querySelector('#quoteInput')
 
+async function handleSubmit(e) {
+  e.preventDefault()
+  const data = quoteInput.value
+  console.log(data)
+
+  await fetch('http://localhost:3001/quote-form', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({data})
+  })
+
+}
 function goBackToMainPage() {
   window.location.href = '../';
 }
 
 goBackBtn.addEventListener('click', goBackToMainPage);
+quoteForm.addEventListener('submit', handleSubmit);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = 3001
 
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/quote-list', (req, res) => {
   res.sendFile(__dirname + '/public/assets/quote-list/quote-list.html')
@@ -15,6 +16,11 @@ app.get('/db/quotes.json', (req, res) => {
 app.get('/quote-form', (req, res) => {
   res.sendFile(__dirname + '/public/assets/quote-form.html')
 })
+
+app.post('/quote-form', (req, res)=>{
+  const data = req.body
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running http://localhost:${PORT}`)
