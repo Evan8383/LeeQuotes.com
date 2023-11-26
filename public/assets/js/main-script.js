62b0e831e9f8c73a57f8fe1cd39da655cbb522ca
Evan <evanbennett83@gmail.com>
2023-11-25 19:28:59 -0500

    Added fetch request to main page. Fetch request completes a get request to the quotes.json file.

--- public/assets/js/main-script.js
+++ public/assets/js/main-script.js
@@ -4,109 +4,118 @@ const quoteContainer = document.getElementById('quoteContainer');
 const quoteList = document.getElementById('full-list-btn');
 const goBackBtn = document.getElementById('go-back-btn');
 
-const quotes = [
-    {
-        quote: `Banana`
-    },
-    {
-        quote: `Meatloaf`
-    },
-    {
-        quote: `Banana\nMeatloaf`
-    },
-    {
-        quote: `For Funsies`
-    },
-    {
-        quote: `Bueller… \nBueller… \nBueller…`
-    },
-    {
-        quote: `That's just how\nfunctions\nwork`
-    },
-    {
-        quote: `We put code in your\ncode so you can\ncode while\nyou code`
-    },
-    {
-        quote: `DO NOT MIX \nPROMISES & \nCALLBACKS.\nIt is a source\nof pain`
-    },
-    {
-        quote: `Vanilla css is\ngood practice`
-    },
-    {
-        quote: `Questions, Concerns,\n Threats...\nCompliments?\nno? ok.`
-    },
-    {
-        quote: `You guys look\nburnt out`
-    },
-    {
-        quote: `Give yourself\na patented\nPOB`
-    },
-    {
-        quote: `10 points for\ngryffindor`
-    },
-    {
-        quote: `Code Smell`
-    },
-    {
-        quote: `Clear as\nmud?`
-    },
-    {
-        quote: `Don't panic`
-    },
-    {
-        quote: `Objection!\nLeading the\nwitness`
-    },
-];
+// const quotes = [
+//   {
+//     quote: `Banana`
+//   },
+//   {
+//     quote: `Meatloaf`
+//   },
+//   {
+//     quote: `Banana\nMeatloaf`
+//   },
+//   {
+//     quote: `For Funsies`
+//   },
+//   {
+//     quote: `Bueller… \nBueller… \nBueller…`
+//   },
+//   {
+//     quote: `That's just how\nfunctions\nwork`
+//   },
+//   {
+//     quote: `We put code in your\ncode so you can\ncode while\nyou code`
+//   },
+//   {
+//     quote: `DO NOT MIX \nPROMISES & \nCALLBACKS.\nIt is a source\nof pain`
+//   },
+//   {
+//     quote: `Vanilla css is\ngood practice`
+//   },
+//   {
+//     quote: `Questions, Concerns,\n Threats...\nCompliments?\nno? ok.`
+//   },
+//   {
+//     quote: `You guys look\nburnt out`
+//   },
+//   {
+//     quote: `Give yourself\na patented\nPOB`
+//   },
+//   {
+//     quote: `10 points for\ngryffindor`
+//   },
+//   {
+//     quote: `Code Smell`
+//   },
+//   {
+//     quote: `Clear as\nmud?`
+//   },
+//   {
+//     quote: `Don't panic`
+//   },
+//   {
+//     quote: `Objection!\nLeading the\nwitness`
+//   },
+// ];
+async function getRandomQuote() {
+  const response = await fetch('/db/quotes.json')
+  const data = await response.json()
+  data.quotes
 
+}
 // Displays a random quote on top of the image
-function showRandomQuote() {
-    // Choose a random quote
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+async function showRandomQuote() {
+  // Choose a random quote
+  
+  const response = await fetch('/db/quotes.json')
+  const data = await response.json()
 
-    // Create Quote
-    const displayQuote = document.createElement('p');
-    displayQuote.setAttribute('id', 'quote');
-    displayQuote.innerHTML = randomQuote.quote.replace(/\n/g, '<br>');
+  const randomIndex = Math.floor(Math.random() * data.quotes.length);
+  const randomQuote = data.quotes[randomIndex];
 
-    // Display Quote
-    quoteContainer.style.opacity = '1';
-    quoteContainer.style.display = 'block';
-    displayQuote.setAttribute('class', 'fade');
-    quoteContainer.appendChild(displayQuote);
+  // Create Quote
+  const displayQuote = document.createElement('p');
+  displayQuote.setAttribute('id', 'quote');
+  displayQuote.innerHTML = randomQuote.replace(/\n/g, '<br>');
 
-    setTimeout(() => {
-        displayQuote.classList.remove('fade');
-    }, 750);
+  // Display Quote
+  quoteContainer.style.opacity = '1';
+  quoteContainer.style.display = 'block';
+  displayQuote.setAttribute('class', 'fade');
+  quoteContainer.appendChild(displayQuote);
+
+  setTimeout(() => {
+    displayQuote.classList.remove('fade');
+  }, 750);
 };
 
 function clearQuote() {
-    quoteContainer.textContent = '';
+  quoteContainer.textContent = '';
 }
 
 // Makes image shake when clicked
 function shakeImage(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    clearQuote();
+  clearQuote();
 
-    image.classList.add('shake');
+  image.classList.add('shake');
 
-    setTimeout(() => {
-      image.classList.remove('shake');
-    }, 700);
+  setTimeout(() => {
+    image.classList.remove('shake');
+  }, 700);
 
-    showRandomQuote();
+  showRandomQuote();
 };
 
 // Buttons to go between html's
 function goToQuoteList() {
-    window.location.href = './quote-list';
+  window.location.href = './quote-list';
 }
 
 // Event Listeners
 image.addEventListener('click', shakeImage);
 image.addEventListener('dblclick', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 });
 quoteList.addEventListener('click', goToQuoteList);
